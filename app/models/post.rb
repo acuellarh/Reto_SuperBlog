558d173fb1292ffa4addcb62ae5f2fe980395e3f
Alejandro <acuellarh@gmail.com>
2019-05-21 21:01:53 -0500

    CRUD for Post Updated and Validation to create a New Post in Spanish

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :title, presence: { message: "Debe escribir un título"} 
+  validates :content, length: {minimum: 250, 
+    too_short: "El numero de caracteres debe ser mayor a %{count}"  } 
+
+  # https://hackernoon.com/performing-custom-validations-in-rails-an-example-9a373e807144
+
+
+
 end
+
