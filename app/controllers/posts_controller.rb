fa78e86e0f7ff1c30c4ed7c0f2f56c4877b42767
Alejandro <acuellarh@gmail.com>
2019-05-25 12:20:33 -0500

    Just show index and show views for users that in not authenticated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :current_post, only: [:show, :edit, :update, :destroy]
   # Identificar el post por el id en los metodos anteriores, evitar repetir en los 4 métodos
 
