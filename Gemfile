f6a81e821eccd37778dcebf114ce19e78b97e1d6
Alejandro <acuellarh@gmail.com>
2019-06-25 18:37:26 -0500

    Markdown for post content

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem 'jquery-rails'
 # Gem Device
 gem 'devise'
 
+# Gem redcarpet for markdown use
+gem 'redcarpet', '~> 3.4'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,27 @@ module ApplicationHelper
     default_url = "http://mysite.com/myavatar.png" # Acá puedo poner una ruta para una imagen propia.
     default_url = "retro"
     url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
-  end    
+  end  
+  
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 
 end
